e940e897ee6db83b6b9d354dd320772b529bafb6
vadimrevich <vadimrevich21@gmail.com>
2020-08-23 20:08:09 +0400

    first commit

---
+++ Windows/SOURCE-MAIN/pub1/Util/getip_1.cmd
@@ -0,0 +1,3 @@
+@echo off
+rem curl http://ipinfo.io/ip > %PUB1%\Util\MyIp.txt
+curl http://ident.me > %PUB1%\Util\MyIp.txt
---
+++ Windows/SOURCE-MAIN/pub1/Util/minifilters-interface-restore.cmd
@@ -0,0 +1,2 @@
+@%SystemRoot%\System32\icacls.exe SystemRoot%\system32\fltLib.dll /restore "fltlib.dll.acl">>setup.log 2>>setup.log
+
